9eb61d0ab1253a5cdbb560290bae81baca52118f
Victoria Musyoka <victoria.musyoka@student.moringaschool.com>
2022-06-26 19:39:47 +0300

    fetch data from local server

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,12 @@ document.addEventListener("DOMContentLoaded",()=>{
     fetchTrips();
     
 })
+
+
+//fetch from json
+function fetchTrips(){
+    fetch("http://localhost:3000/trips")
+    .then(resp=>resp.json())
+    .then(tripsData=>tripsData.forEach(data=>displayTrips(data)))
+    
+}
